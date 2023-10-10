a8e803f85da09c81bf4189252cb0286d32f6905d
twhlynch <twhlynch@gmail.com>
2023-10-10 20:35:33 +1100

    new page!!!!11!!11!!1

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var cursorOuter = document.getElementById("cursor-outer");
-var cursorInner = document.getElementById("cursor-inner");
-var links = document.getElementsByTagName("a");
+let cursorOuter = document.getElementById("cursor-outer");
+let cursorInner = document.getElementById("cursor-inner");
+let links = document.getElementsByTagName("a");
 
 document.addEventListener("mousemove", function (e) {
     cursorInner.style.transform =
         cursorOuter.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
 
-for (var i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("mouseover", () => {
         cursorOuter.classList.add("cursor-outer-hover");
         cursorInner.classList.add("cursor-inner-hover");
@@ -18,6 +18,30 @@ for (var i = 0; i < links.length; i++) {
     });
 }
 
-particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
+const currentTime = document.getElementById("currentTime");
+const currentState = document.getElementById("currentState");
+const time = new Date();
+
+const options = { timeZone: 'Australia/Melbourne' };
+const localTime = time.toLocaleString('en-US', options);
+
+let hour = new Date(localTime).getHours();
+
+if (hour >= 17 && hour < 24) {
+    currentState.innerHTML = "awake";
+} else if (hour >= 9 && hour < 17) {
+    currentState.innerHTML = "working";
+} else {
+    currentState.innerHTML = "sleeping";
+}
+let minutes = new Date(localTime).getMinutes();
+currentTime.innerHTML = `${hour < 12 ? hour : hour - 12}:${minutes < 10 ? '0' + minutes : minutes} ${hour >= 12 ? "PM" : "AM"}`;
+    
+function changeStyle() {
+    document.documentElement.style.setProperty('--text-color-default', "#b7b8c5");
+    document.documentElement.style.setProperty('--text-color-alt', "#e0e1f0");
+    document.documentElement.style.setProperty('--background-color-default', "#1d1d1d");
+    document.documentElement.style.setProperty('--background-color-alt', "#131c22");
+    document.documentElement.style.setProperty('--text-color-link', "#329e9e");
+    document.documentElement.style.setProperty('--border-color', "#262626");
+}
