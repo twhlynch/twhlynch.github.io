a70a5b1791c1b362351ab4874cbb56365ccb0c67
Tom Lynch <twhlynch@gmail.com>
2026-03-23 23:59:43 +1100

    chore: add nvmrc

--- .nvim.lua
+++ .nvim.lua
@@ -1,5 +1,8 @@
-vim.keymap.set("n", "<leader>B", function()
-	local command = '$SHELL -i -c "open http://localhost:5173/ && nvm use 22 && npm run dev"'
+local command = '$SHELL -i -c "open http://localhost:5173/ && nvm use && npm run dev"'
+
+local build = function()
 	vim.fn.system("tmux kill-window -t npm 2>/dev/null")
 	vim.fn.system("tmux new-window -d -n npm '" .. command .. "'")
-end, { desc = "Build & Run" })
+end
+
+vim.keymap.set("n", "<leader>B", build, { desc = "Build & Run" })
